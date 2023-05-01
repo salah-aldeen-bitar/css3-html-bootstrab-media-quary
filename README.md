0f636858e277f77ef83a47bd7968e408a2b00b5b
salah-aldeen-bitar <so-danger@hotmail.com>
2023-05-01 20:45:50 +0300

    css3-html-bootstrab-media-quary

---
+++ README.md
@@ -0,0 +1,2 @@
+# css3-html-bootstrab-media-quary
+# css3-html-bootstrab-media-quary
---
+++ js/scribt.js
@@ -0,0 +1,53 @@
+//$(document).ready(function(){
+   // $('i.icon').click(function(){
+     //   $('nav-list').slideToggle()
+    //});
+    //when scroll body
+   /* $(window).scroll(function(){
+        var sc = $(this).scrollTop();
+        if(sc > 100){
+          $('.navbar').addClass('sticky');
+              $('.row').addClass('sticky')
+        }
+        else
+        {
+            $('.navbar').removeClass('sticky');
+              $('.row').removeClass('sticky')
+        }
+    });*/
+
+    
+    
+//});
+//www.themealdb.com/api/json/v1/1/filter.php?i={searchTerm}
+//www.themealdb.com/api/json/v1/1/lookup.php?i={id}
+/* let input5 = document.querySelector('.input1');
+ let btn5 = document.querySelector('#btn-1');
+ let show5 = document.querySelector('.show');
+
+btn5.addEventListener('click' , getRecipes);
+function getRecipes(){
+    let  searchTerm = input5.ariaValueMax.trim();
+     let apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTerm}';
+     fetch (apiUrl)
+     .then(res=>{
+      console.log(res);
+    }) 
+}*/
+
+/*document.querySelector('.js-btn').onclick = function(){
+  document.querySelector('.show').innerHTML ='salah';
+};
+window.onload = function(){
+  alert("hello salah");
+};
+var input1 = document.querySelector('.input1');
+var show = document.querySelector('.show');
+   input1.onkeyup = function(){
+    show.innerHTML = 'heloooooo';
+    console.log("helooooo");
+   };*/
+   
+
+  
+   
